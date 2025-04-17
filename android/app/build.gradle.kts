0fa8f4edbaddcef83002d6cd1828d0f25853bcb0
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-17 14:40:59 +0200

    Update .gitignore and configure signing for release builds in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,12 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+def keystoreProperties = new Properties()
+def keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(new FileInputStream(keystorePropertiesFile))
+}
+
 android {
     namespace = "com.dubdev.dallalat"
     compileSdk = flutter.compileSdkVersion
@@ -30,12 +36,30 @@ android {
         versionName = flutter.versionName
     }
 
-    buildTypes {
+    signingConfigs {
         release {
+            keyAlias keystoreProperties['keyAlias']
+            keyPassword keystoreProperties['keyPassword']
+            storeFile keystoreProperties['storeFile'] ? file(keystoreProperties['storeFile']) : null
+            storePassword keystoreProperties['storePassword']
+        }
+    }
+
+    buildTypes {
+       // release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        //}
+        release {
+            signingConfig signingConfigs.release
+            minifyEnabled true // Enables shrinking, obfuscation, and optimization
+            shrinkResources true // Removes unused resources
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
+    }
+     lintOptions {
+        abortOnError false // Prevents lint errors from stopping the build
     }
 }
 
